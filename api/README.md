427a22966d4e63c74fd72570470da15821f77026
rathoremayank <phoenix@jenkins-artifactory-server.us-central1-a.c.seismic-iridium-280908.internal>
2020-12-05 18:35:47 +0000

    added readme files

---
+++ api/README.md
@@ -0,0 +1,11 @@
+# How to run the Rest API
+
+### Running from python3
+python3 main.py
+
+### Running via Flask
+export FLASK_APP=app.py
+flask run
+
+### Running on a custom port
+flask run --port=8080
--- api/config.py
+++ api/config.py
@@ -3,7 +3,7 @@ from flaskext.mysql import MySQL
 
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'webdamn'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Welcome01'
+app.config['MYSQL_DATABASE_DB'] = 'phoenix-db'
+app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
 mysql.init_app(app)
